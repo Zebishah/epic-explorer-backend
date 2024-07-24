b67e357ae5b4bcd1ee3fa13dba2a643bc3825264
Zebishah <zebihaider123@gmail.com>
2024-07-24 21:24:27 +0500

    changes

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Allow requests from this origin
+    origin: "https://epic-explorer-frontend-9rtk.vercel.app/", // Allow requests from this origin
     credentials: true, // Allow cookies to be sent with the request
   },
 });
@@ -42,7 +42,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow requests from this origin
+    origin: "https://epic-explorer-frontend-9rtk.vercel.app/", // Allow requests from this origin
     credentials: true, // Allow cookies to be sent with the request
   })
 );
